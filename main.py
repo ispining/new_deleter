8414605262bf9ecd88dbda0d29392d8b685b530c
Anonymous <ilvi142014@gmail.com>
2022-11-06 23:56:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ import os
 import threading
 import time
 import config
-import window
 from config import bot, pickle
 
 
 
 config.loop_bots_threads()
 
-th = threading.Thread(target=window.start_window)
-th.daemon = True
-th.start()
-threading.main_thread()
+
 
 while True:
     try:
